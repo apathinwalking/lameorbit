d7bd2ebce1df11b16f4442af9c2a3ad890d432e0
Poco Umami <poco@localhost.localdomain>
2015-11-12 15:57:51 -0500

    reorganized

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,53 @@
 var gulp = require('gulp');
-var download = require('gulp-download');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var Promise = require('bluebird');
+var _ = require('lodash');
+var request = require('request');
+var icsSrc = require('./data/ics/ics-sources.json').sources;
+var ical2json = require('ical2json')
+var data = require('gulp-data');
+var jeditor = require('gulp-json-editor');
+var swig = require('gulp-swig');
+var paths = {
+	"downloadedICS":"tmp/*.ics"
+}
 
-gulp.task('download-ics')
+//TODO: all icals in the data folder
+
+gulp.task('default',['make-tmp-folder','download-ics','ics-to-json']);
+
+gulp.task('test',function(){
+	return gulp.src('tmp/*.ics')
+	.pipe(data(function(file){
+		var data = fs.readFileSync(file.path,"utf-8");
+		return ical2json.convert(data)
+	}))
+	.pipe(jeditor())
+	.pipe(gulp.dest('tmp'));
+});
+
+gulp.task('make-tmp-folder',function(){
+	return mkdirp.sync('tmp/',function(err){
+		if(err) console.error(err)
+	});
+});
+
+gulp.task('download-ics',function(){
+	return _.map(icsSrc,function(x){
+		return request(x.url)
+			.pipe(fs.createWriteStream("tmp/"+x.name+".ics"))
+	});
+});
+
+gulp.task('ics-to-json',function(){
+	return _.map(icsSrc,function(x){
+		var filename = "tmp/"+x.name+".ics";
+		var outfile = "tmp/"+x.name+".json";
+		console.log(filename);
+		return gulp.src(paths.downloadedICS)
+			.pipe(function(data){
+				console.log(data);
+			});
+	});
+});
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 #Lameorbit: ics calendar map display
 
-If you want to load data from ics sources via webaddresses, copy the ex/ics-sources.json file, fill it in, and place it in /data/ics, make the data folder if you need to.
+If you want to load data from ics sources via webaddresses, copy the ex/ics-sources.json file, fill it in, and place it in /data/ics/
+
+Additionally you can place any icals you want in the data/ics folder and they will be added to the database
